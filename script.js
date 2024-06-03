448367e76990e93caea3bb93ed74ad9fb3e1f405
satishrathod-dev <167071223+satishrathod-dev@users.noreply.github.com>
2024-06-03 15:28:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+	document.addEventListener('DOMContentLoaded', function() {
+		let inputs = document.getElementsByClassName('code');
+
+		for(let i=0; i<inputs.length; i++){
+			inputs[i].addEventListener('input', function() {
+				let nextInput = this.nextElementSibling;
+
+				if(this.value.length === 1 && nextInput){
+					nextInput.focus();
+				}
+			});
+			inputs[i].addEventListener('keydown', function(event) {
+				if(event.key === 'Backspace' && this.value.length === 0) {
+					let prevInput = this.previousElementSibling;
+
+					if(prevInput) {
+						prevInput.focus();
+					}
+				}
+			});
+		}
+	});
+})
